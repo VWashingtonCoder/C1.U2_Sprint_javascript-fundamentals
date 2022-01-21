5fe8c987ab36ebd82585b1e39446cfb52b198818
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-01-21 05:00:42 -0700

    Topic 1:Tasks 1 & 2 Complete_Tests 3/24 Passed

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function myFunction() {
 myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+/* nestedFunction can access the variable Internal thanks to the nested function still being in the function scope of myFunction.
+  Thanks to scope & closure, we are able to let the function (nestedFunction) access the variable internal. */
 
 
 
@@ -28,11 +29,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  let counter = 0
+  for (let i = 0; i < num+1; i++){
+    counter = counter + i;
   }
+  return counter
+}
  
+console.log(summation(4));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
